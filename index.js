6dd53e0c2df506620a3635ded3e76547bc91fc9f
Rodias <gohouerodias@gmail.com>
2022-08-16 02:32:04 +0100

    oki

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ console.log(player)
 
 function animate() {
     window.requestAnimationFrame(animate)
-    c.fillStyle = 'black'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    player.update()
-    ennemy.update()
+    /* c.fillStyle = 'black'
+     c.fillRect(0, 0, canvas.width, canvas.height)
+     player.update()
+     ennemy.update()*/
     console.log('oki')
 }
 
