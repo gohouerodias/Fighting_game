e4934781a9aeb4938d6bf65c6d2238fb31a49263
Rodias <gohouerodias@gmail.com>
2022-08-16 19:21:01 +0100

    commit

--- index.js
+++ index.js
@@ -22,12 +22,13 @@ class Sprite {
 
     update() {
         this.draw()
-        this.velocity.y += gravity
-        this.position.y += this.velocity;
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y;
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0
-        }
+        } else this.velocity.y += gravity
     }
 }
 
@@ -56,6 +57,19 @@ const ennemy = new Sprite({
 
 
 console.log(player)
+const keys = {
+    q: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    z: {
+        pressed: false
+    }
+}
+
+let lastKey
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -63,8 +77,63 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     ennemy.update()
-    console.log('oki')
+
+    player.velocity.x = 0
+    if (keys.q.pressed && lastKey === 'q') {
+        player.velocity.x = -1
+
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1
+
+    }
+    else if (keys.z.pressed && lastKey === 'z') {
+        player.velocity.y = 5
+
+    }
 }
 
 animate()
 
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break;
+        case 'q':
+            keys.q.pressed = true
+            lastKey = 'q'
+            break;
+
+        case 'z':
+            /*  keys.z.pressed = true
+              lastKey = 'z'*/
+            player.velocity.y = -10
+            break;
+
+        default:
+            break;
+    }
+
+    console.log(event.key)
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break;
+        case 'q':
+            keys.q.pressed = false
+            break;
+
+        case 'z':
+            keys.z.pressed = false
+            break;
+
+        default:
+            break;
+    }
+
+    console.log(event.key)
+})
